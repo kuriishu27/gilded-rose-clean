2589b4a9998e435fd9b8e5e26fd9bde70d889fbe
Christian Ray Leovido <christian.leovido@gmail.com>
2020-10-23 11:31:08 +0100

    add initial files and setup

---
+++ GildedRose-Kata/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  GildedRose-Kata
+//
+//  Created by Christian Leovido on 23/10/2020.
+//
+
+import Foundation
+
+public struct Item: Equatable {
+
+	var name: String
+	var sellIn: Int
+	var quality: Int
+
+}
